8c2c473a4a2375d86e6935858268b694a631a414
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2026-01-20 17:58:12 +0200

    Initial Commit

---
+++ AssistantMP/AssistantMPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantMPApp.swift
+//  AssistantMP
+//
+//  Created by Roman Tverdokhleb on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AssistantMPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
